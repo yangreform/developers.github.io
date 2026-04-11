cef41e1eb69e1c3941970d80a3db5f34599bd814
yangreform <36433111+yangreform@users.noreply.github.com>
2026-04-11 20:34:15 +0800

    Add files via upload

--- LandlordSG/flutter_bootstrap.js
+++ LandlordSG/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3025941360" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2788052526" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
