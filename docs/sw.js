2265cca71bd6d251c7ddeb195e699012bbf1fb74
jacky <prof@weishun.cc>
2026-07-19 20:37:46 +0800

    chore(pwa): add service worker, pwa meta tags and seo open graph image

---
+++ docs/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'landlordsg-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './heatmap.html',
+  './about.html',
+  './manifest.json',
+  './icons/Icon-192.png',
+  './icons/Icon-512.png',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
